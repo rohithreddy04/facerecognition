9468da85d249f70ece890804a9de5781477d9e18
Rohith Reddy <reddy.rohith09@gmail.com>
2020-05-17 21:42:44 -0700

    face-recognition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+const particlesOptions = {
+  particles: {
+   number: {
+    value: 30,
+    density: {
+      enable: true,
+      value_area: 800
+    }
+
+   }
+  }
+}
+
+class App extends Component{
+  constructor() {
+      super();
+      this.state = {
+        input: '',
+      };
+    }
+
+    onInputChange = (event) => {
+      console.log(event.target.value);
+    };
+
+    onSubmit = () => {
+      app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
   );
+      console.log('click');
+    }
+    render() {return (<div className="App">
+      <Particles className='particles' params={particlesOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+
+    </div>);
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+           <div className='white f3'>
+               {'Rohith, you current rank is...'}
+           </div>
+           <div>
+           <div className='white f1'>
+               {'#5'}
+           </div>
+           </div>
+        </div>
+    )
+}
+
+export default Rank;
